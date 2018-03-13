9f5a87ac24e920234679016f51e27aca73b62c2b
Maurice de Jong <20231852+mauricejuuhh@users.noreply.github.com>
2018-03-13 14:30:28 +0100

    nhg

--- app/etc/config.php
+++ app/etc/config.php
@@ -2,6 +2,7 @@
 return array (
   'modules' => 
   array (
+    'Betalingsmethodecheck_Betalingsmethode' => 1,
     'Magento_Store' => 1,
     'Magento_Directory' => 1,
     'Magento_AdvancedPricingImportExport' => 1,
