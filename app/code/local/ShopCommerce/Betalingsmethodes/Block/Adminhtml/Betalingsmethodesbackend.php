fff22c6f6728521185e080cfb8f0d250a27cdb5a
JobGroen <meneergroen@outlook.com>
2018-03-13 17:07:49 +0100

    .

--- app/code/local/ShopCommerce/Betalingsmethodes/Block/Adminhtml/Betalingsmethodesbackend.php
+++
@@ -1,5 +0,0 @@
-<?php  
-
-class ShopCommerce_Betalingsmethodes_Block_Adminhtml_Betalingsmethodesbackend extends Mage_Adminhtml_Block_Template {
-
-}
--- app/code/local/ShopCommerce/Betalingsmethodes/Helper/Data.php
+++
@@ -1,5 +0,0 @@
-<?php
-class ShopCommerce_Betalingsmethodes_Helper_Data extends Mage_Core_Helper_Abstract
-{
-}
-	 
