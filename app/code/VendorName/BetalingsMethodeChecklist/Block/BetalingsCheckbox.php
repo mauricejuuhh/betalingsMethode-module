41bee899fa8b9654a252dd66a31506e1af349897
Maurice de Jong <20231852+mauricejuuhh@users.noreply.github.com>
2018-03-13 10:52:05 +0100

    .

---
+++ app/code/VendorName/BetalingsMethodeChecklist/Block/BetalingsCheckbox.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace VendorName\BetalingsMethodeChecklist\Block;
+
+class BetalingsCheckbox
+{
+
+    /**
+     * @return string
+     */
+    public function betalingsCheckbox()
+    {
+        //Your block code
+        return 'Hello World!';
+    }
+}
